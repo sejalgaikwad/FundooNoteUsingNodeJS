63c3e9792c88cd25332563be977ce3b3dab1561f
sejalgaikwad <sejalgaikwad24@gmail.com>
2020-06-04 13:57:44 +0530

    Defining Schema for user using mongoose

---
+++ app/model/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+// defining the mongodb schema
+var userSchema = new schema({                                  
+    firstname: {
+        type: String,
+        require: [true, "First Name can't be Empty"],
+        trim: true
+    },
+
+    lastname: {
+        type: String,
+        require: [true, "Last Name can't be Empty"],
+        trim: true
+    },
+
+    email: {
+        type: String,
+        require: [true, "Email can't be Empty"],
+        trim: true,
+        lowercase: true,
+        unique: [true, 'Email already exists'],
+    },
+
+    password: {
+        type: String,
+        require: [true, "Password can't be Empty"],
+        trim: true
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+}, 
+{
+    timestamps: true
+});
+module.exports = mongoose.model('usermodel', userSchema);              
